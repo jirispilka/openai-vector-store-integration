fbe091135cabb17f651574dcc8afacd62230fc5a
Jiri Spilka <jirka.spilka@gmail.com>
2024-04-16 16:43:05 +0200

    update documentation, change file updates flow (create files, detach assistant files, attach new files, delete files)

--- src/main.py
+++ src/main.py
@@ -38,51 +38,90 @@ async def main() -> None:
             data = [d for d in data if d]
 
         data = await split_data_if_required(data, encoding)
+        file_ids_to_delete = await get_file_ids_to_delete(client, aid, assistant)
 
-        # The files are first deleted to circumvent OpenAI Assistant files limit.
-        # It can happen that for a short period of time the Assistant is without any file attached.
-        await delete_files(client, aid, assistant)
-
+        # 1 - Create files
         store = await Actor.open_key_value_store()
 
-        # Create assistant files
+        files_created = []
         for i, d in enumerate(data):
             try:
-                filename = (
-                    f"{aid.file_prefix}_{aid.dataset_id}_{i}.json" if aid.file_prefix else f"{aid.dataset_id}_{i}.json"
-                )
+                prefix = f"{aid.file_prefix}_{aid.dataset_id}" if aid.file_prefix else f"{aid.dataset_id}"
+                filename = f"{prefix}_{i}.json"
                 file = await client.files.create(file=(filename, json.dumps(d).encode("utf-8")), purpose="assistants")
                 Actor.log.debug("Created file %s, file_id: %s", file.filename, file.id)
-                await client.beta.assistants.files.create(aid.assistant_id, file_id=file.id)
-                Actor.log.debug("Attached file %s, file_id: %s to the assistant", file.filename, file.id)
                 await store.set_value(filename, json.dumps(d), content_type="application/json")
                 Actor.log.debug("Stored the file in Actor's key value store file: %s", file.filename)
+                files_created.append(file)
             except Exception as e:
                 Actor.log.exception(e)
 
+        # 2 - detach existing files from the assistant
+        await detach_assistant_files(client, aid.assistant_id, file_ids_to_delete)
 
-async def delete_files(client, aid: OpenaiAssistantFilesIntegrationInputs, assistant) -> None:
+        # 3 - attach new files
+        await attach_assistant_files(client, aid, files_created)
 
-    # only delete files that ara attached to the assistant_id
+        # delete files
+        await delete_files(client, file_ids_to_delete)
+
+
+async def attach_assistant_files(client, assistant_id, files_created):
+    for file in files_created:
+        try:
+            await client.beta.assistants.files.create(assistant_id, file_id=file.id)
+            Actor.log.debug("Attached file %s, file_id: %s to the assistant", file.filename, file.id)
+        except Exception as e:
+            Actor.log.exception(e)
+
+
+async def get_file_ids_to_delete(client, aid: OpenaiAssistantFilesIntegrationInputs, assistant) -> set:
+    """Only delete files that are associated with the assistant_id."""
+
     files_to_delete = set(aid.file_ids_to_delete) if aid.file_ids_to_delete else set()
     files_to_delete = files_to_delete.intersection(set(assistant.file_ids))
 
-    # delete files with prefix attached to the assistant_id
-    files_to_delete = files_to_delete.union(await get_assistant_files_by_prefix(client, aid))
+    # get files with prefix associated with the assistant_id
+    return files_to_delete.union(await get_assistant_files_by_prefix(client, aid))
 
-    if files_to_delete:
-        Actor.log.debug("Files to delete: %s (also including file_prefix: %s files)", files_to_delete, aid.file_prefix)
-        for file_id in files_to_delete:
-            try:
-                await client.beta.assistants.files.delete(file_id, assistant_id=aid.assistant_id)
-                await client.files.delete(file_id)
-                Actor.log.debug("Deleted file_id: %s", file_id)
-            except Exception as e:
-                Actor.log.exception(e)
+
+async def detach_assistant_files(client: AsyncOpenAI, assistant_id: str, files_to_delete: set) -> None:
+    """Detach files from the assistant.
+
+    Note that file is not actually deleted. Only the association is removed.
+
+    https://platform.openai.com/docs/api-reference/assistants/deleteAssistantFile
+    """
+
+    for file_id in files_to_delete:
+        try:
+            await client.beta.assistants.files.delete(file_id, assistant_id=assistant_id)
+            Actor.log.debug("Detached file_id: %s", file_id)
+        except Exception as e:
+            Actor.log.exception(e)
+
+
+async def delete_files(client: AsyncOpenAI, files_to_delete: set) -> None:
+    """
+    Delete OpenAI files.
+
+    https://platform.openai.com/docs/api-reference/files/delete
+    """
+
+    Actor.log.debug("Files to delete: %s", files_to_delete)
+    for file_id in files_to_delete:
+        try:
+            await client.files.delete(file_id)
+            Actor.log.debug("Deleted file_id: %s", file_id)
+        except Exception as e:
+            Actor.log.exception(e)
 
 
 async def get_assistant_files_by_prefix(client: AsyncOpenAI, aid: OpenaiAssistantFilesIntegrationInputs) -> set | None:
-    """List Assistant files and return file_ids that starts with prefix."""
+    """List Assistant files and return file_ids that starts with prefix.
+
+    https://platform.openai.com/docs/api-reference/assistants/listAssistantFiles
+    """
 
     if not aid.file_prefix:
         return
