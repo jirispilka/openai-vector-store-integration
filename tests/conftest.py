c4ab533d9a9926f445bde147f1138f695231d606
Jiri Spilka <jirka.spilka@gmail.com>
2024-07-02 10:30:07 +0200

    Correct unit-tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,28 +1,32 @@
 from typing import AsyncGenerator
+import random
 
 import openai
 import pytest
 from dotenv import load_dotenv
 from openai.types import FileObject
 from openai.types.beta import VectorStore
+from string import ascii_lowercase
 
 load_dotenv()
 
 client = openai.AsyncClient()
 
-FILE_PREFIX = "unittest_"
 
-
-@pytest.fixture
+@pytest.fixture(scope="function")
 async def vector_store_fixture() -> AsyncGenerator[VectorStore, None]:
-    vector_store = await client.beta.vector_stores.create(name="unittest_vector_store")
+
+    suffix = "".join(random.choices(ascii_lowercase, k=8))
+    vector_store = await client.beta.vector_stores.create(name=f"unittest_vector_store_{suffix}")
     yield vector_store
     await client.beta.vector_stores.delete(vector_store.id)
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 async def file_fixture() -> AsyncGenerator[FileObject, None]:
-    filename = f"{FILE_PREFIX}file.txt"
+
+    s = "".join(random.choices(ascii_lowercase, k=4))
+    filename = f"unittest_{s}__file.txt"
     filedata = b"Hello, OpenAI!"
 
     file = await client.files.create(file=(filename, filedata), purpose="assistants")
